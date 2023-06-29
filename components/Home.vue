6994d52707ea473ffe19fca70d586a8b954a4a0e
falana sheriff <falanasheriffdeen@gmail.com>
2023-06-29 20:17:18 +0100

    continue

---
+++ components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full h-20 border-2 border-green-900 p-6 bg-red-400"  >
+        <p class="w-30"  >loud</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <Tutorial/>
+  <!-- <Tutorial/> -->
+  <Home />
 </template>
 
 <script>
