8987f3c67b71b90cc5158d48a6fef8520d9ec085
falana sheriff <falanasheriffdeen@gmail.com>
2023-07-01 10:50:36 +0100

    nuxt testing

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nav class="w-70 h-20 bg-red-300 border-4 border-green-800 p-4">
+            <p>dddd</p>
+        </nav>
+        <div>
+            <slot />
+        </div>
+        <footer class="w-60 h-50 bg-green-300">
+
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'default'
+}
+</script>
